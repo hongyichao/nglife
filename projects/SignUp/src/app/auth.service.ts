12913fcca1dfb574bf846682c964856a02c77c4b
lucaschao@outlook.com <lucaschao@outlook.com>
2020-05-28 19:27:35 +1000

    Signup:saveinstorage

--- projects/SignUp/src/app/auth.service.ts
+++ projects/SignUp/src/app/auth.service.ts
@@ -5,20 +5,29 @@ import { User } from './user-register';
   providedIn: 'root'
 })
 export class AuthService {
-  users: User[] = [{Id: 1, FirstName: 'John1', LastName: 'Z', Email: 'john1z@abc.com', Password: 'Password1'},
-  {Id: 2, FirstName: 'John2', LastName: 'Z', Email: 'john2z@abc.com', Password: 'Password2'},
-  {Id: 3, FirstName: 'John3', LastName: 'Z', Email: 'john3z@abc.com', Password: 'Password3'}
-];
-  constructor() { }
+  users: User[];
+  constructor() {
+    const storageUsers = sessionStorage.getItem('signUpUsers');
+    this.users = storageUsers ? JSON.parse(storageUsers) : [];
+
+    if (!this.users) {
+      this.users = [{Id: 1, FirstName: 'John1', LastName: 'Z', Email: 'john1z@abc.com', Password: 'Password1'},
+      {Id: 2, FirstName: 'John2', LastName: 'Z', Email: 'john2z@abc.com', Password: 'Password2'},
+      {Id: 3, FirstName: 'John3', LastName: 'Z', Email: 'john3z@abc.com', Password: 'Password3'}
+      ];
+    }
+   }
 
   addUser(newUser: User) {
     newUser.Id = this.users.length + 1;
     this.users.push(newUser);
+    sessionStorage.setItem('signUpUsers', JSON.stringify(this.users));
   }
 
   updateUser(editedUser: User) {
     const userIndex = this.users.findIndex( u => u.Id === editedUser.Id);
     this.users.splice(userIndex, 1, editedUser);
+    sessionStorage.setItem('signUpUsers', JSON.stringify(this.users));
   }
 
   getUserById(id: number) {
